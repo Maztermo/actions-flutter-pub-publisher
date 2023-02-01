0414e27026bf7d2a97a0a3f76586b8d38d8b88a8
Martin Jenssen <martinkontakter@gmail.com>
2023-02-01 13:37:28 +0100

    attempt 10

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 cd /docker-action
 echo "creating docker image with Flutter version: $INPUT_FLUTTER_VERSION"
 
+inp
 # here we can make the construction of the image as customizable as we need
 # and if we need parameterizable values it is a matter of sending them as inputs
-docker build -t docker-action --build-arg flutter_version=$INPUT_FLUTTER_VERSION . && docker run docker-action
+docker build -t docker-action --build-arg flutter_version=$INPUT_FLUTTER_VERSION INPUT_CREDENTIAL=$INPUT_CREDENTIAL INPUT_PACKAGE_DIRECTORY=$INPUT_PACKAGE_DIRECTORY INPUT_SKIP_TEST=$INPUT_SKIP_TEST INPUT_FLUTTER_PACKAGE=$INPUT_FLUTTER_PACKAGE EXECUTABLE_PREFIX=$EXECUTABLE_PREFIX INPUT_DRY_RUN=$INPUT_DRY_RUN . && docker run docker-action
